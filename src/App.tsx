eb40a0e778ce964d6dd9131a05b28406b9f13510
Toyin Muhammed <toyinmuhammed50@gmail.com>
2023-04-04 23:05:32 +0200

    Fix: .env

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <div className="app">
             <h1><img src={logo} alt="logo" /> Food Generator</h1>
-            <p className="app__subtitle">Welcome, hacker!</p>
+            <p className="app__subtitle">Welcome, Hacker!</p>
             <p className="app__description">Wanna have a meal out?</p>
             <Random />
         </div>
